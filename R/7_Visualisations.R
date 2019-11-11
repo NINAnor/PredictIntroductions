c8f6e03cae8ba7ba07efdcbba3c332d8148d1cfc
Sam Perrin <sam.perrin@ntnu.no>
2019-11-11 16:33:03 +0100

    Added new graphics

--- R/7_Visualisations.R
+++ R/7_Visualisations.R
@@ -26,11 +26,15 @@ analytics <- readRDS(paste0("./Data/Species_Data/",gsub(' ','_',focal_species),"
 
 model_data <- all_data$raw_data %>% filter(native == 0)
 
+# Get uncertainty variables
 Uncertainty <- analytics$intervals$width
 InitProbs <- analytics$intervals$mean
 
+# Put together one data frame to contain all predictive information
 all_data_likelihoods <- cbind(model_data, lake_likelihoods_wIncrease,lake_likelihoods_noIncrease,  Uncertainty,InitProbs)
 all_data_likelihoods$changeWithTemp <- all_data_likelihoods$lake_likelihoods_wIncrease - all_data_likelihoods$lake_likelihoods_noIncrease
+
+# Set variables as factors, may be needed for maps later
 all_data_likelihoods$UncertaintyLevel <- as.factor(cut(all_data_likelihoods$Uncertainty, c(0, 0.01, 0.05, 0.15, 1.2),
                                                        labels = c("negligible","very low", "low","moderate")))
 all_data_likelihoods$PredictedIntro_wIncrease <- as.factor(cut(all_data_likelihoods$lake_likelihoods_wIncrease, c(-0.01, 0.05, 0.2, 0.5, 1.01),
@@ -38,13 +42,12 @@ all_data_likelihoods$PredictedIntro_wIncrease <- as.factor(cut(all_data_likeliho
 all_data_likelihoods$PredictedIntro_noIncrease <- as.factor(cut(all_data_likelihoods$lake_likelihoods_noIncrease, c(-0.01, 0.05, 0.2, 0.5, 1.01),
                                                                labels = c("very low", "low","moderate","high")))
 
-
 # Set minimum and maximum values for change in temp
 min_tempChange_1 <- floor(min(all_data_likelihoods$changeWithTemp)/0.05)*0.05
 max_tempChange_1 <- ceiling(max(all_data_likelihoods$changeWithTemp)/0.05)*0.05
 intervals <- (max_tempChange_1 - min_tempChange_1)/4
 
-
+# Save data for use in QGIS
 saveRDS(all_data_likelihoods, file=paste0("./Data/Species_Data/",gsub(' ','_',focal_species),"/introduction_likelihoods.RDS"))
 write.csv(all_data_likelihoods, file=paste0("./Data/Species_Data/",gsub(' ','_',focal_species),"/introduction_likelihoods.csv"))
 
@@ -52,18 +55,26 @@ print("Saved mapping data, moving on to introductions maps")
 
 #---------------------------------------------------------------------#
 # 7B. Start mapping
+#
+# The first of these maps will show average likelihood of establishment
+# in each hexagon for each species. Hexagons in which the species was
+# already established before the scenario ran are marked black.
+# Species native distributions are denoted by the grey polygons.
+#
 #---------------------------------------------------------------------#
 
 # Produce a map of Norway to use
 Norway<-getData("GADM", country="NO", level=0)
 Norway1<-getData("GADM", country="NO", level=1)
+
+# Need to get it down to just the southern counties
 Norway1_sub<-Norway1[!(Norway1@data$NAME_1 %in% c('Troms', 'Finnmark', 'Nordland')),]
 par(mar=c(1,1,1,1))
 Norway_df <- fortify(Norway)
 Norway1_sub_df <- fortify(Norway1_sub)
 Norway1_sub_sf <- st_as_sf(Norway1_sub)
 
-# Get native distribution now
+# Get native distribution for everythign except Rainbow Trout
 if(focal_species != "Oncorhynchus mykiss") {
   hk_distribution_map <- readRDS("Data/native_distribution.rds")
   species_native <- hk_distribution_map[hk_distribution_map$canonicaln == focal_species & 
@@ -72,20 +83,17 @@ if(focal_species != "Oncorhynchus mykiss") {
   species_native_intersection_union <- st_union(species_native_intersection)
 }
 
-# These are just elements to clear the grid
-theme_opts <- list(theme(panel.grid.minor = element_blank(), panel.grid.major = element_blank(), panel.background = element_blank(), plot.background = element_blank(), axis.line = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank(), axis.ticks = element_blank(), axis.title.x = element_blank(), axis.title.y = element_blank(), plot.title = element_blank()))
 
 # Let's first plot the initial appearances
 if (focal_species != "Oncorhynchus mykiss") {
 
   # Now let's plot predicted appearances without temp increase, using likelihood of introduction 
-  # over our set threshold
+  # as our value to average over hexagons
   predicted_appearances_noIncrease <- ggplot(data=all_data_likelihoods) + 
     stat_summary_hex(aes(x = decimalLongitude, 
                          y = decimalLatitude,
                          z = lake_likelihoods_noIncrease),
                      bins=n_bins) +
-    # theme_opts +
     scale_fill_gradient2(mid="yellow", high="red",
                          breaks=seq(0,1,0.25), #breaks in the scale bar
                          limits=c(0,1)) +
@@ -100,15 +108,13 @@ if (focal_species != "Oncorhynchus mykiss") {
     xlab("Longitude") +
     ylab("Latitude")
   
-  
   # Now let's plot predicted appearances with temp increase, using likelihood of introduction 
-  # over our set threshold
+  # as our value to average over hexagons
   predicted_appearances_wIncrease <- ggplot(data=all_data_likelihoods) + 
     stat_summary_hex(aes(x = decimalLongitude, 
                          y = decimalLatitude,
                          z = changeWithTemp),
                      bins=n_bins) +
-    # theme_opts +
     scale_fill_gradient2(low="blue", high="green",
                          breaks=seq(min_tempChange_1,max_tempChange_1,intervals), #breaks in the scale bar
                          limits=c(min_tempChange_1,max_tempChange_1)) +
@@ -123,12 +129,12 @@ if (focal_species != "Oncorhynchus mykiss") {
     xlab("Longitude") +
     ylab("Latitude")
 } else {
-  predicted_appearances_noIncrease <- ggplot(data=all_data_likelihoods) + 
+    # This situation is just for rainbow trout. Only change is it gets rid of the native distribution bit.
+    predicted_appearances_noIncrease <- ggplot(data=all_data_likelihoods) + 
     stat_summary_hex(aes(x = decimalLongitude, 
                          y = decimalLatitude,
                          z = lake_likelihoods_noIncrease),
                      bins=n_bins) +
-    # theme_opts +
     scale_fill_gradient2(mid="yellow", high="red",
                          breaks=seq(0,1,0.25), #breaks in the scale bar
                          limits=c(0,1)) +
@@ -142,11 +148,7 @@ if (focal_species != "Oncorhynchus mykiss") {
     xlab("Longitude") +
     ylab("Latitude")
   
-  
-  # Now let's plot predicted appearances with temp increase, using likelihood of introduction 
-  # over our set threshold
   predicted_appearances_wIncrease <- ggplot(data=all_data_likelihoods) +
-    # geom_polygon(data=Norway1_sub_df, aes(long,lat,group=group), fill="grey") +
     stat_summary_hex(aes(x = decimalLongitude,
                          y = decimalLatitude,
                          z = changeWithTemp),
@@ -168,6 +170,16 @@ if (focal_species != "Oncorhynchus mykiss") {
 
 print("Plotted predicted introductions, moving onto basins.")
 
+#---------------------------------------------------------------------#
+# 7C. Map water basin averages
+#
+# These maps will now show the average likelihood of establishment 
+# across entire water basins, excluding lakes which already had 
+# establishments before the scenarios were run. As with the above maps,
+# the second graph shows changes with introduction of a temperature
+# change.
+#
+#---------------------------------------------------------------------#
 
 # Now let's plot averages for water basins
 if (download_basins == TRUE) {
@@ -181,11 +193,15 @@ if (download_basins == TRUE) {
                        options="-c search_path=\"Hydrography\""
   )
   con_basin <- poolCheckout(pool_basin)
+  
+  # Download basin geometries
   basin_import <- tbl(con_basin,"waterregions_dem_10m_nosefi") %>%
     dplyr::select(gid, geom) %>%
     filter(gid %in% !!all_data_likelihoods$eb_waterregionID) %>%
     collect()
   basin_list <- vector()
+  
+  # Convert text strings into geometries
   for(i in seq_len(nrow(basin_import))) {
     wkb_int <- structure(list(basin_import$geom[i]), class = "WKB")
     basin_list <- c(basin_list,wkb_int)
@@ -194,29 +210,32 @@ if (download_basins == TRUE) {
   basin_geom_sf <- basin_geom %>%
     st_sf %>%
     st_cast
+  
+  # Give each object a name and save to file
   basin_geom_sf$eb_waterregionID <- basin_import$gid
   saveRDS(basin_geom_sf,file="./Data/waterRegions.rds")
 } else {
   basin_geom_sf <- readRDS("./Data/waterRegions.rds")
 }
 
+# Get just the lakes which didn't already have presences
 all_data_likelihoods_basinTransform <- all_data_likelihoods %>%
   filter(presence == 0)
 
+# Find average likelihoods for basins
 basin_aggregate <- all_data_likelihoods %>%
   group_by(eb_waterregionID) %>%
   summarise(avg_noIncrease = mean(lake_likelihoods_noIncrease),
             avg_wIncrease = mean(lake_likelihoods_wIncrease))
 
+# Match up the crs's
 basin_geom_sf <- st_transform(basin_geom_sf, 4326)
-# inOut_native <- st_intersects(basin_geom_sf,species_native_intersection_union)
-# basin_geom_sf$native <- lengths(inOut_native)
 
 basin_plot_data <- merge(basin_geom_sf,basin_aggregate,all.x=TRUE,by="eb_waterregionID")
 basin_plot_data$change <- basin_plot_data$avg_wIncrease - basin_plot_data$avg_noIncrease
 # basin_plot_data$average <- ifelse(basin_plot_data$present == 1 | basin_plot_data$native == 1, 1, basin_plot_data$avg_noIncrease)
 
-
+# And plot
 basin_map_noIncrease <- ggplot(data=basin_plot_data) +
   geom_sf(mapping = aes(fill=avg_noIncrease)) +
   scale_fill_gradient(low="yellow", high="red") +
@@ -224,7 +243,7 @@ basin_map_noIncrease <- ggplot(data=basin_plot_data) +
   xlab("Longitude") +
   ylab("Latitude")
   
-
+# And plot with temperature increase
 basin_map_wIncrease <- ggplot(data=basin_plot_data) +
   geom_sf(mapping = aes(fill=change)) +
   scale_fill_gradient2(low="blue", high="green") +
@@ -232,8 +251,16 @@ basin_map_wIncrease <- ggplot(data=basin_plot_data) +
   xlab("Longitude") +
   ylab("Latitude")
 
-# Let's first plot the initial appearances
 
+#---------------------------------------------------------------------#
+# 7D. Internal validation plots
+#
+# We now get a couple of plots which show us uncertainty levels for
+# the lakes which had introductions
+#
+#---------------------------------------------------------------------#
+
+# Let's first plot the initial appearances
 Internal_Uncertainty <- ggplot(data=all_data_likelihoods[all_data_likelihoods$introduced == 1,]) + 
   geom_polygon(data=Norway1_sub_df, aes(long,lat,group=group), fill="grey") +
   geom_point(aes(x = all_data_likelihoods[all_data_likelihoods$introduced == 1,"decimalLongitude"], 
@@ -244,7 +271,6 @@ Internal_Uncertainty <- ggplot(data=all_data_likelihoods[all_data_likelihoods$in
   labs(subtitle=paste0("Current distribution of ",focal_species))
 
 
-# Let's first plot the initial appearances
 
 Internal_Prediction <- ggplot(data=all_data_likelihoods[all_data_likelihoods$introduced == 1,]) + 
   geom_polygon(data=Norway1_sub_df, aes(long,lat,group=group), fill="grey") +
